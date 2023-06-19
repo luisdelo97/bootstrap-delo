04fa8788be7fc4247294b310534e91f73773c520
Luis Delorenzi <ldelorenzi@fiuna.edu.py>
2023-06-19 16:09:52 -0400

    Terminando el proyecto en Bootstrap, con una ultima funcionalidad en el menu desplegable

---
+++ js/main.js
@@ -0,0 +1,8 @@
+const bsCollapse = new bootstrap.Collapse("#navbarNavDropdown", {
+  toggle: false,
+});
+
+document.addEventListener("click", (e) => {
+  if (!e.target.matches(".dropdown-menu a")) return false;
+  bsCollapse.hide();
+});
